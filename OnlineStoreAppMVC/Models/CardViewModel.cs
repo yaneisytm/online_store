3fa5e7406ef4d3dad77adcbec07da78f2b424412
m1gu3 <94m1gu3@gmail.com>
2021-08-11 01:32:34 +0200

    new validation for card payment and success view added

--- OnlineStoreAppMVC/Models/CardViewModel.cs
+++ OnlineStoreAppMVC/Models/CardViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
+using OnlineStoreAppMVC.Models.ValidationAttributes;
 
 namespace OnlineStoreAppMVC.Models
 {
@@ -11,7 +12,7 @@ namespace OnlineStoreAppMVC.Models
         [Required]
         [CreditCard]
         [Display(Name = "Card Number")]
-        [StringLength(16, ErrorMessage="Invalid card number length")]
+        [StringLength(16, ErrorMessage="Invalid card number length.")]
         public string CardNumber { get; set; }
 
         [Required]
@@ -20,16 +21,19 @@ namespace OnlineStoreAppMVC.Models
 
         [Required]
         [Display(Name = "Expiration Month")]
-        [StringLength(2, ErrorMessage = "Invalid month length")]
+        [IntegerParse("Not a valid month.")]
+        [StringLength(2, ErrorMessage = "Invalid month length.")]
         public string ExpMonth { get; set; }
 
         [Required]
         [Display(Name = "Expiration Year")]
-        [StringLength(2, ErrorMessage = "Invalid year length")]
+        [NotExpiredCard]
+        [StringLength(2, ErrorMessage = "Invalid year length.")]
         public string ExpYear { get; set; }
 
         [Required]
-        [StringLength(5, ErrorMessage = "Invalid cvc", MinimumLength=3)]
+        [IntegerParse("Not a valid CVC.")]
+        [StringLength(4, ErrorMessage = "Invalid CVC.", MinimumLength=3)]
         public string CVC { get; set; }
 
         [Required]
